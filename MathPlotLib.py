8198905dd5cc9cce400e3ab06f811fd9f8b01421
Shree <22cs100@nandhaengg.org>
2026-01-05 18:32:17 +0530

    Mathplotlib functions

---
+++ MathPlotLib.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+x=[1,2,3]
+y=[1,2,3]
+# plt.plot(x,y)
+# plt.bar(x,y,color='green')
+# plt.scatter(x,y,color='pink')
+# plt.hist(x,y)
+plt.pie(x,y)
+plt.title("Simple line plot")
+plt.xlabel("X - Axis")
+plt.ylabel("Y - Axis")
+plt.show()
